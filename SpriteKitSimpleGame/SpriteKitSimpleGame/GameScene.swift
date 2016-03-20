ef24089eebb96e600394755473db6b97eb1a529b
Subcipher <krishna@javaworkgroup.com>
2016-03-19 22:16:11 -0400

    ABCswift

--- SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -8,6 +8,40 @@
 
 import SpriteKit
 
+    func + (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    }
+
+    func - (left: CGPoint, right: CGPoint) -> CGPoint {
+        return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+
+    func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+        return CGPoint(x: point.x * scalar, y: point.y * scalar)
+    }
+
+    func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+        return CGPoint(x: point.x / scalar, y: point.y / scalar)
+    }
+
+    #if !(arch(x86_64) || arch(arm64))
+        func sqrt(a: CGFloat) -> CGFloat{
+            return CGFloat(sqrtf(Float(a)))
+        }
+        #endif
+
+    extension CGPoint {
+        func length() -> CGFloat {
+            return sqrt(x * x + y * y)
+        }
+        func normalized() -> CGPoint {
+            return self / length()
+            
+        }
+    }
+
+
+
 class GameScene: SKScene {
     
     //1
@@ -20,10 +54,17 @@ class GameScene: SKScene {
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         //
         addChild(player)
-    }
-}
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            ))
 
-func random() -> CGFloat{
+        }
+ 
+
+func random() -> CGFloat {
     return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
 }
 
@@ -32,34 +73,32 @@ func random(min min: CGFloat, max: CGFloat) -> CGFloat {
 }
 
 func addMonster() {
-    //Create sprite
+    
+    // Create sprite
     let monster = SKSpriteNode(imageNamed: "monster")
     
-    //Determine where to spawn the monster along the Y axis
-   let actualY = random(min: monster.size.height/2, max: monster.size.height - monster.size.height/2)
+    // Determine where to spawn the monster along the Y axis
+    let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
     
-    //Position the monster slighly off-screen along the right edge
-    //amd along a random position along the Y axis as calculated above
-    monster.position = CGPoint(x: monster.size.width + monster.size.width/2, y: actualY)
+    // Position the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
     
-    //Add the monster to the scene
-     addChild(monster)
+    // Add the monster to the scene
+    addChild(monster)
     
-    //Determine speed of the monster
-    let actualDuration = random(min:CGFloat(2.0),max: CGFloat(4.0))
+    // Determine speed of the monster
+    let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
     
-    //create the action
-    let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY),duration: NSTimeInterval(actualDuration))
+    // Create the actions
+    let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
     let actionMoveDone = SKAction.removeFromParent()
     monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+    
+    
+    }
 
-    
-    
 }
-
-
-
-
 
 
 
